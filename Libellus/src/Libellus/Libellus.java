3ed5c6b800590d3d02a9f728c99dcf75d6cc4355
Katherine Jamieson (kmj098) <kmj098@mail.usask.ca>
2014-10-01 11:39:56 -0600

    Adding all current files for libellus system

---
+++ Libellus/src/Libellus/Libellus.java
@@ -0,0 +1,17 @@
+package Libellus;
+
+/**
+ *
+ * @author Brina
+ */
+public class Libellus
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
+    }
+}
